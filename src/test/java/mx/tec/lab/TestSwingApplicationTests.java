e130c08a6dbcfa783557b2823a1d677b5b0a5719
Alejandro Aguiano <alex1203@live.com.mx>
2021-05-07 00:09:34 -0600

    tests

--- src/test/java/mx/tec/lab/TestSwingApplicationTests.java
+++ src/test/java/mx/tec/lab/TestSwingApplicationTests.java
@@ -1,13 +1,40 @@
 package mx.tec.lab;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest 
 class TestSwingApplicationTests {
-
-	@Test
-	void contextLoads() {
+	
+	TestSwingApplication application = new TestSwingApplication();
+	
+	@Test 
+	void givenTwoIntegers_WhenSum_ThenCorrectResult() {
+		// Given 5 and 7
+		application.numberOneTextField.setText("5");
+		application.numberTwoTextField.setText("7");
+		String expectedResult = "12";
+		// When operation is sum
+		application.operationButton.doClick();
+		// Then result is 12
+		String actualResult = application.resultTextField.getText();
+		assertEquals(expectedResult, actualResult);
 	}
-
+	
+	@Test 
+	void givenInvalidInput_WhenSum_ThenInvalidInputCleared_AndNoResulProvided() {
+		// Given a and 7
+		application.numberOneTextField.setText("a");
+		application.numberTwoTextField.setText("7");
+		String expectedResult = "";
+		String expectedResult2 = "";
+		// When operation is sum
+		application.operationButton.doClick();
+		// Then result is empty
+		String actualResult = application.resultTextField.getText();
+		assertEquals(expectedResult, actualResult);
+		String actualResult2 = application.numberOneTextField.getText();
+		assertEquals(expectedResult2, actualResult2);
+	}
+	
 }
